387fcd9d695fdd0985fc9f8865a4c71855ffa494
JwstFeed <122465126+JwstFeed@users.noreply.github.com>
2023-05-21 22:19:49 +0300

    Create ObservationGenerator.cs

---
+++ JwstScheduleProvider/BL/ObservationGenerator.cs
@@ -0,0 +1,95 @@
+using Infrastructure.Extensions;
+using JwstScheduleProvider.Model;
+
+namespace JwstScheduleProvider.BL;
+
+internal class ObservationGenerator
+{
+    #region Data Members
+    private static string dateFormat { get; } = "yyyy-MM-dTHH:mm:ssZ";
+    private IDictionary<string, int> keys { get; }
+    #endregion
+
+    #region Ctor
+    public ObservationGenerator()
+    {
+        this.keys = new Dictionary<string, int>()
+        {
+            { "VisitID", 0 },
+            { "PcsMode", 1 },
+            { "VisitType", 2 },
+            { "ScheduledStartTime", 3 },
+            { "Duration", 4 },
+            { "ScienceInstumentAndMode", 5 },
+            { "TargetName", 6 },
+            { "Category", 7 },
+            { "KeyWords", 8 },
+        };
+    }
+    #endregion
+
+    #region Public Methods
+    public IObservation GenerateMultiObjectObservation(string[] observationRow)
+        =>
+        new MultiObjectObservation()
+        {
+            ScienceInstumentAndMode = observationRow[keys["ScienceInstumentAndMode"]],
+            ScheduledStartTime = getScheduledStartTime(observationRow),
+            TargetName = observationRow[keys["TargetName"]],
+            ClusterIndex = getClusterIndex(observationRow),
+            VisitType = observationRow[keys["VisitType"]],
+            Duration = observationRow[keys["Duration"]],
+            PcsMode = observationRow[keys["PcsMode"]],
+            VisitID = getVisitID(observationRow)
+        };
+
+    public IObservation GenerateNormalObservation(string[] observationRow)
+        =>
+        new Observation()
+        {
+            ScienceInstumentAndMode = observationRow[keys["ScienceInstumentAndMode"]],
+            ScheduledStartTime = getScheduledStartTime(observationRow),
+            TargetName = observationRow[keys["TargetName"]],
+            ClusterIndex = getClusterIndex(observationRow),
+            VisitType = observationRow[keys["VisitType"]],
+            Category = observationRow[keys["Category"]],
+            Duration = observationRow[keys["Duration"]],
+            KeyWords = observationRow[keys["KeyWords"]],
+            PcsMode = observationRow[keys["PcsMode"]],
+            VisitID = getVisitID(observationRow)
+        };
+
+    public IObservation GenerateRealTimeCommandObservation(string[] observationRow)
+        =>
+        new RealTimeCommandObservation()
+        {
+            ScienceInstumentAndMode = observationRow[keys["ScienceInstumentAndMode"]],
+            ScheduledStartTime = getScheduledStartTime(observationRow),
+            ClusterIndex = getClusterIndex(observationRow),
+            VisitType = observationRow[keys["VisitType"]],
+            Duration = observationRow[keys["Duration"]],
+            PcsMode = observationRow[keys["PcsMode"]],
+            VisitID = getVisitID(observationRow)
+        };
+    #endregion
+
+    #region Private Methods
+    private string getClusterIndex(string[] observationRow)
+    {
+        string unixStartTime = getScheduledStartTime(observationRow).ToUnixTime();
+        string visitID = getVisitID(observationRow);
+
+        return $"{unixStartTime}_{visitID}";
+    }
+
+    private DateTime getScheduledStartTime(string[] observationRow)
+        =>
+        observationRow[keys["ScheduledStartTime"]]
+        .ToDateTime(dateFormat)
+        .ToUniversalTime();
+
+    private string getVisitID(string[] observationRow)
+        =>
+        observationRow[keys["VisitID"]];
+    #endregion
+}
