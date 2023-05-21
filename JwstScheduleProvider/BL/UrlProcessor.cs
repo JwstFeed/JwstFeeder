bde585b04e8908a5cd9dd18ae463eb1ac88f93b1
JwstFeed <122465126+JwstFeed@users.noreply.github.com>
2023-05-21 22:21:21 +0300

    Update UrlProcessor.cs

--- JwstScheduleProvider/BL/UrlProcessor.cs
+++ JwstScheduleProvider/BL/UrlProcessor.cs
@@ -1,4 +1,3 @@
-﻿using Infrastructure.Extensions;
 using JwstScheduleProvider.Extensions;
 using JwstScheduleProvider.Model;
 
@@ -7,83 +6,63 @@ namespace JwstScheduleProvider.BL;
 internal class UrlProcessor
 {
     #region Data Members
-    private static string dateFormat { get; } = "yyyy-MM-dTHH:mm:ssZ";
-    private IDictionary<string, int> keys { get; }
-    private int validRowNumber { get; }
+    private ObservationGenerator observationGenerator { get; }
+    private int realTimeCommandColumnsNumber { get; }
+    private int multiObjectColumnsNumber { get; }
+    private int validColumnsNumber { get; }
     #endregion
 
     #region Ctor
     public UrlProcessor()
     {
-        this.validRowNumber = 9;
-
-        this.keys = new Dictionary<string, int>()
-        {
-            { "VisitID", 0 },
-            { "PcsMode", 1 },
-            { "VisitType", 2 },
-            { "ScheduledStartTime", 3 },
-            { "Duration", 4 },
-            { "ScienceInstumentAndMode", 5 },
-            { "TargetName", 6 },
-            { "Category", 7 },
-            { "KeyWords", 8 },
-        };
+        this.observationGenerator = new ObservationGenerator();
+        this.realTimeCommandColumnsNumber = 7;
+        this.multiObjectColumnsNumber = 8;
+        this.validColumnsNumber = 9;
     }
     #endregion
 
     #region Public Methods
-    public IEnumerable<Observation> GetObservationSchedule(string url)
+    public IEnumerable<IObservation> GetObservationSchedule(string url)
         =>
         url
         .GetUrlContent()
         .ToScheduleTable()
         .Select(parseObservation)
-        .Where(isRowNotEmpty);
+        .Where(isNotEmptyObservation);
     #endregion
 
     #region Private Methods
-    private Observation parseObservation(string observation)
+    private IObservation parseObservation(string observation)
     {
         string[] observationRow = observation.ToScheduleTableRow();
-        bool isValidScheduleRow = observationRow.Length == this.validRowNumber;
 
-        return isValidScheduleRow
-            ? new Observation()
-            {
-                ScienceInstumentAndMode = observationRow[keys["ScienceInstumentAndMode"]],
-                ScheduledStartTime = getScheduledStartTime(observationRow),
-                TargetName = observationRow[keys["TargetName"]],
-                ClusterIndex = getClusterIndex(observationRow),
-                VisitType = observationRow[keys["VisitType"]],
-                Category = observationRow[keys["Category"]],
-                Duration = observationRow[keys["Duration"]],
-                KeyWords = observationRow[keys["KeyWords"]],
-                PcsMode = observationRow[keys["PcsMode"]],
-                VisitID = getVisitID(observationRow)
-            }
-            : new Observation();
+        return observationRow.Length switch
+        {
+            int value when value == this.realTimeCommandColumnsNumber => getRealTimeCommandObservation(observationRow),
+            int value when value == this.multiObjectColumnsNumber => getMultiObjectObservation(observationRow),
+            int value when value == this.validColumnsNumber => getNormalObservation(observationRow),
+            _ => new EmptyObservation()
+        };
     }
 
-    private string getClusterIndex(string[] observationRow)
-    {
-        string unixStartTime = getScheduledStartTime(observationRow).ToUnixTime();
-        string visitID = getVisitID(observationRow);
-
-        return $"{unixStartTime}_{visitID}";
-    }
-
-    private DateTime getScheduledStartTime(string[] observationRow)
+    private IObservation getRealTimeCommandObservation(string[] observationRow)
         =>
-        observationRow[keys["ScheduledStartTime"]]
-        .ToDateTime(dateFormat);
+        this.observationGenerator
+        .GenerateRealTimeCommandObservation(observationRow);
 
-    private string getVisitID(string[] observationRow)
+    private IObservation getMultiObjectObservation(string[] observationRow)
         =>
-        observationRow[keys["VisitID"]];
+        this.observationGenerator
+        .GenerateMultiObjectObservation(observationRow);
 
-    private bool isRowNotEmpty(Observation observation)
+    private IObservation getNormalObservation(string[] observationRow)
         =>
-        observation.TargetName != null;
+        this.observationGenerator
+        .GenerateNormalObservation(observationRow);
+
+    private bool isNotEmptyObservation(IObservation observation)
+        =>
+        observation is not EmptyObservation;
     #endregion
 }
