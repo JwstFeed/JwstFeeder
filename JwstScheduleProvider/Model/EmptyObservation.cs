05206ca46a3a52c194c1e7f2bb694989fe7f2166
JwstFeed <122465126+JwstFeed@users.noreply.github.com>
2023-05-21 22:22:30 +0300

    Create EmptyObservation.cs

---
+++ JwstScheduleProvider/Model/EmptyObservation.cs
@@ -0,0 +1,24 @@
+namespace JwstScheduleProvider.Model;
+
+internal class EmptyObservation : IObservation
+{
+    public string ClusterIndex => string.Empty;
+
+    public string VisitID => string.Empty;
+
+    public string PcsMode => string.Empty;
+
+    public string VisitType => string.Empty;
+
+    public DateTime ScheduledStartTime => DateTime.UtcNow;
+
+    public string Duration => string.Empty;
+
+    public string ScienceInstumentAndMode => string.Empty;
+
+    public string TargetName => string.Empty;
+
+    public string Category => string.Empty;
+
+    public string KeyWords => string.Empty;
+}
